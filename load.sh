b15e12a4220f72e72087c2e0850621539eb62d20
Steven Barnett <steven.abarnett@gmail.com>
2016-08-11 12:40:55 +0000

    Still a work in progress, but looking nice

--- load.sh
+++ load.sh
@@ -1,5 +1,10 @@
+# WIDTH passed to all tmux status bar scripts for responsiveness
+WIDTH=$1
+SMALL=80
+MEDIUM=140
+
 # Determine CPU load
-pcpu=$(ps -Ao pcpu | awk '{sum = sum + $1}END{print sum}')
+pcpu=$(ps -Ao pcpu | awk '{sum = sum + $1}END{printf "%4.1f", sum}')
 
 # Determine MEM load
 mem=$(ps -Ao rss | awk '{sum = sum + $1}END{print sum}')
@@ -19,7 +24,36 @@ human_print() {
   [ $TB -lt 16384 ] && echo "${TB} T" && return
 }
 
+print_bars() {
+  local int=${1%.*}
+  local cur=$(($int*10/$2))
+  local cnt=0
+  while [ $cur -gt 0 ]; do
+#    [ $cnt -lt 3 ] && tput setaf 2
+#    [ $cnt -gt 2 -a $cnt -lt 7 ] && tput setaf 3
+#    [ $cnt -gt 5 ] && tput setaf 1
+    echo -n '|'
+    cnt=$(($cnt + 1))
+    cur=$(($cur - 1))
+  done
+#  tput sgr0
+  while [ $cnt -lt 10 ]; do
+    echo -n ' '
+    cnt=$(($cnt + 1))
+  done
+}
+
 # Output: CPU $pcpu [|||   ] - MEM $mem / $tmem [|||   ]
-echo CPU $pcpu \[\|\|\| \] - MEM $(human_print $mem) / $(human_print $tmem) \[\|\|\| \]
-#echo $mem / $tmem = $(($mem * 10 / $tmem))
-#printf "|%.0s" {1..$pcpu}
+if [ $WIDTH -gt $MEDIUM ]; then
+  # "Large" view
+  echo "CPU $pcpu [$(print_bars $pcpu 100)] - MEM $(human_print $mem) / $(human_print $tmem) [$(print_bars $mem $tmem)]"
+else
+  if [ $WIDTH -gt $SMALL ]; then
+    # "Medium" view
+    echo "CPU: $pcpu% - MEM: $(human_print $mem) / $(human_print $tmem)"
+  else
+    # "Small" view
+    pmem=$(awk -v m=$mem -v t=$tmem 'BEGIN {printf "%3.1f", m*100/t}')
+    echo "CPU $pcpu% MEM $pmem%"
+  fi
+fi
